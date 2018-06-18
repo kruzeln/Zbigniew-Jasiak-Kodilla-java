c59e796ec9fe0da4d601c9910177dd7f48411efd
Zbigniew Jasiak <kruzeln@interia.pl>
2018-06-18 16:21:29 +0200

    commit

--- src/main/java/com/kodilla/KodillaCourseApplication.java
+++ src/main/java/com/kodilla/KodillaCourseApplication.java
@@ -1,12 +1,14 @@
  package com.kodilla;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.*;
 
 @SpringBootApplication
 public class KodillaCourseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KodillaCourseApplication.class, args);
+
     }
+
+
 }
