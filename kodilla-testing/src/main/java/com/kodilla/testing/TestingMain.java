840b618f6a6d41a759c7f87d1d51c854b6a5c73d
Zbigniew Jasiak <kruzeln@interia.pl>
2018-06-19 14:04:19 +0200

    Zadanie

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.testing;
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main (String[] args){
+        SimpleUser simpleUser = new SimpleUser("TheForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("TheForumUser")){
+            System.out.println("Test OK!");
+        } else {
+            System.out.println("Error!");
+        }
+        // drugi test - dla calculator
+        Calculator kalku = new Calculator(25, 15);
+        if ((kalku.add() == 40) && (kalku.subtract() == 10)){
+            System.out.println("Test kalkulatora OK!");
+        }
+        else {System.out.println("Test kalkulatora failed!");}
+
         System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
     }
 }
